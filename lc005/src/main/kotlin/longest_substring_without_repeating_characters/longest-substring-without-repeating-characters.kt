c2b1e2cc65a9a88b495b912036762e044a622ba4
WarmthDawn <warmth06@sina.com>
2022-03-20 19:55:21 +0800

    :sparkles: 假装今天写了

---
+++ lc005/src/main/kotlin/longest_substring_without_repeating_characters/longest-substring-without-repeating-characters.kt
@@ -0,0 +1,22 @@
+package longest_substring_without_repeating_characters
+import kotlin.math.max
+
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        val lastIndexes = mutableMapOf<Char, Int>()
+        var bg = -1
+        var result = 0
+        for (i in s.indices) {
+            val ch = s[i]
+            val chi = lastIndexes[ch]
+            if (chi != null) {
+                bg = max(bg, chi)
+            } else {
+            }
+            result = max(result, i - bg)
+            lastIndexes[ch] = i
+
+        }
+        return result;
+    }
+}
