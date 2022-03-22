40dc158a934f0fc4a3c6076bf1b5babf8413e190
WarmthDawn <warmth06@sina.com>
2022-03-22 22:11:22 +0800

    :bug: 改一下包名

--- lc005/src/main/kotlin/add_minimu_number_of_rungs/add-minimum-number-of-rungs.kt
+++ lc005/src/main/kotlin/add_minimu_number_of_rungs/add-minimum-number-of-rungs.kt
@@ -1,3 +1,5 @@
+package add_minimu_number_of_rungs
+
 class Solution {
     fun addRungs(rungs: IntArray, dist: Int): Int {
         return (sequenceOf(0) + rungs.asSequence()).windowed(2).map { (a, b) -> (b - a - 1) / dist }.sum()
